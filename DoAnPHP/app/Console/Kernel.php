e966eccc17d5bc23220fe4bd1100d949da8915ed
duydinh <dinhpham972@gmail.com>
2020-06-04 21:54:14 +0700

    Quản lý nhà cung cấp

--- DoAnPHP/app/Console/Kernel.php
+++ DoAnPHP/app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\CusstomPHP\Backup;
+use App\Http\Controllers\SaoLuuController;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +15,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        // Commands\Inspire::class,
     ];
 
     /**
@@ -24,18 +26,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
-    }
-
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
-
-        require base_path('routes/console.php');
+        $schedule->call(function () {
+            Backup::backup_mysql();
+        })->dailyAt('07:00');
     }
 }
